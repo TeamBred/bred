0c60df0236f0ff3153cd7db623f3cf2355c9abaf
Michael Kulinski <me@michaelkulinski.com>
2016-09-09 08:59:19 -0700

    added the expsenses model and population the database

--- server/models/user.js
+++ server/models/user.js
@@ -2,13 +2,12 @@
 const Sequelize = require('sequelize');
 const dbUrl = 'postgres://zqfgesav:s317KdarS_J7BqbMhGuuxtUxEJyRNcmN@elmer.db.elephantsql.com:5432/zqfgesav'
 const sequelize = new Sequelize(dbUrl);
-// const Expense = require('')
+const Expenses = require('./expenses');
 
 
 const User = sequelize.define('user2', {
   username: {
     type: Sequelize.STRING,
-    // field: 'user_name' // Will result in an attribute that is firstName when user facing but first_name in the database
   },
   password: {
     type: Sequelize.STRING
@@ -23,14 +22,14 @@ const User = sequelize.define('user2', {
   freezeTableName: true // Model tableName will be the same as the model name
 });
 
-// User.hasMany(Expense, {as: 'Expenses'})
+User.hasMany(Expenses, {as: 'expenses'});
 
-User.sync({force: true}).then(function () {
-  // Table created
-  // return User.create({
-  //   firstName: 'John',
-  //   lastName: 'Hancock'
-  // });
-});
+// User.sync({force: true}).then(function () {
+//   // Table created
+//   // return User.create({
+//   //   firstName: 'John',
+//   //   lastName: 'Hancock'
+//   // });
+// });
 
 module.exports = User;
