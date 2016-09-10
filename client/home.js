769552d60928b462f927e7d5ee9e9763600a842c
Michael Kulinski <me@michaelkulinski.com>
2016-09-10 16:08:00 -0700

    edited some css

--- client/home.js
+++ client/home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, Link} from 'react-router';
 
 class Home extends React.Component {
   constructor() {
@@ -8,9 +9,19 @@ class Home extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>You home now</h1>
-      </div>
+      <section className="home-main">
+      <video id="bgVideo" autoPlay loop>
+        <source src="./client/coutingmoney.mp4" type="video/mp4" />
+      </video>
+        <div className="home-box">
+          <h1>Bred</h1>
+          <h4>Where your finances come together</h4>
+          <div className="home-buttons">
+            <button className="signup"><Link to="/signup">Signup Today</Link></button>
+            <button className="login"><Link to="/login">Login</Link></button>
+          </div>
+        </div>
+      </section>
     )
   }
 }
